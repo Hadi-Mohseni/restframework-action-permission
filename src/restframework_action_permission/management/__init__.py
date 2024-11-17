b3d16abdf72000704b0c028d371ff591e5534013
Hadi Mohseni <hadimohseni1996@gmail.com>
2024-11-17 06:57:09 +0330

    fix: discard models without model_name

--- src/restframework_action_permission/management/__init__.py
+++ src/restframework_action_permission/management/__init__.py
@@ -9,13 +9,14 @@ def create_permission(sender, **kwargs):
         return
 
     for content_type in ContentType.objects.all():
-        model_name = content_type.model_class()._meta.model_name
-        codename = "list_{}".format(model_name)
-        if not Permission.objects.filter(
-            content_type_id=content_type.id,
-            codename=codename,
-        ).exists():
-            name = "Can list {}".format(model_name)
-            Permission.objects.create(
-                name=name, codename=codename, content_type_id=content_type.id
-            )
+        if content_type.model_class() is not None:
+            model_name = content_type.model_class()._meta.model_name
+            codename = "list_{}".format(model_name)
+            if not Permission.objects.filter(
+                content_type_id=content_type.id,
+                codename=codename,
+            ).exists():
+                name = "Can list {}".format(model_name)
+                Permission.objects.create(
+                    name=name, codename=codename, content_type_id=content_type.id
+                )
